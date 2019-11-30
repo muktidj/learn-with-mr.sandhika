092ed0460a79ede6637ee751d932f71c03f26b47
muktidj <muktidwijatmoko@gmail.com>
2019-11-30 12:21:28 +0700

    DOM Selec 1. Mencoba menggunakan selection getElementById kemeudian memberi css melalui JS seperti color, background, merubah tulisan

---
+++ dom-selection/script.js
@@ -0,0 +1,6 @@
+//DOM SELECTION
+//document.getElementById()
+const judul = document.getElementById("judul");
+judul.style.color = "salmon";
+judul.style.backgroundColor = "black";
+judul.innerHTML = "Mukti DJ"
