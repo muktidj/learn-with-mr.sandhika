3fce1d6eb9070c9b29ae92a5e5e1875d529ea3bb
muktidj <muktidwijatmoko@gmail.com>
2019-11-30 12:49:16 +0700

    DOM Select 3. Mencoba menggunakan select getElementByClassName, kemudian memberi beberapa css melalui JS: color, fontWeight, innerHTML

--- dom-selection/script.js
+++ dom-selection/script.js
@@ -8,7 +8,11 @@ judul.innerHTML = "Mukti DJ";
 // document.getElementByTagName()
 const p = document.getElementsByTagName("p");
 for (let i = 0; i < p.length; i++) {
-
-    p[i].style.backgroundColor = 'lightgreen'
-
+  p[i].style.backgroundColor = "lightgreen";
 }
+
+// document.getElementByClassName() -> HTML Collection
+const p1 = document.getElementsByClassName("p1");
+p1[0].innerHTML = "Diubah dari JS";
+p1[0].style.fontWeight = "bold"
+p1[0].style.color = "black"
