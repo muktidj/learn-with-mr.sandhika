e034e195571066051b576c44bfb5c62afdb1240e
muktidj <muktidwijatmoko@gmail.com>
2019-11-30 12:41:27 +0700

    DOM Sec 2. Mendapatkan tag menggunakan getElementByTagName dan menambahkan css backgroundColor ttag p dengan JS

--- dom-selection/script.js
+++ dom-selection/script.js
@@ -3,4 +3,12 @@
 const judul = document.getElementById("judul");
 judul.style.color = "salmon";
 judul.style.backgroundColor = "black";
-judul.innerHTML = "Mukti DJ"
+judul.innerHTML = "Mukti DJ";
+
+// document.getElementByTagName()
+const p = document.getElementsByTagName("p");
+for (let i = 0; i < p.length; i++) {
+
+    p[i].style.backgroundColor = 'lightgreen'
+
+}
