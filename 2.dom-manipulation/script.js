5840ef963bce4cef1a87436273755b06458a6e63
muktidj <muktidwijatmoko@gmail.com>
2019-12-09 21:17:52 +0700

    2. Mencoba property setAttributre, remove, hingga get

--- 2.dom-manipulation/script.js
+++ 2.dom-manipulation/script.js
@@ -1,4 +1,8 @@
 const judul = document.getElementById("judul");
 judul.innerHTML = "Hello Mukti";
-judul.style.backgroundColor = "grey"
-judul.style.color = "lightgreen"
+judul.style.backgroundColor = "grey";
+judul.style.color = "lightgreen";
+judul.setAttribute("name", "MuktiDJ");
+
+const ahref = document.querySelector("section#a a");
+ahref.removeAttribute('href')
