06ee83c7d2f3d3266a7d385a64b47e2864065020
muktidj <muktidwijatmoko@gmail.com>
2019-12-17 12:21:36 +0700

    4. Mencoba properti createElement && TextNode, appendChild, insertBefore pada script.js

--- 2.dom-manipulation/script.js
+++ 2.dom-manipulation/script.js
@@ -15,3 +15,33 @@ p1.classList.remove('p1')
 
 const p3 = document.querySelector('.p3')
 p1.classList.toggle('p3')
+
+
+// Create Element && Create Text Node
+const newParagraph = document.createElement('p')
+const textNewParagraph = document.createTextNode('New Paragraph')
+
+// appendChild
+const saveTextToNewParagraph = newParagraph.appendChild(textNewParagraph)
+
+// Mau disimpan dimana
+const sectionA = document.getElementById('a')
+ sectionA.appendChild(newParagraph)
+
+//  Create Element && Text Node
+const newList = document.createElement('li')
+const textNewList = document.createTextNode('new list from JS')
+
+// appendChild
+const saveTextToNewList = newList.appendChild(textNewList)
+
+// insertBefore && mau disimpan dimana
+const ul = document.querySelector('section#b ul')
+const insertNewlist = ul.querySelector('ul li:nth-child(2)')
+
+//add
+ul.insertBefore(newList, insertNewlist)
+
+newList.classList.add('newlist')
+newList.style.backgroundColor = 'coral'
+newList.style.color = 'white'
