191f920d3bf29e9d1387d744d95b465d56fd575f
muktidj <muktidwijatmoko@gmail.com>
2019-12-22 14:59:11 +0700

    Bab events, Mencoba addEventLister pada properti ul untuk menambahkan li pada script.js

--- 3.events/script.js
+++ 3.events/script.js
@@ -1,6 +1,8 @@
 const paragraph3 = document.querySelector('.p3')
 const paragraph2 = document.querySelector('.p2')
+const paragraph4 = document.querySelector('section#b p')
 
+// Event Handler
 const ubahWarna3 = () => {
     paragraph3.style.backgroundColor = 'coral'
 }
@@ -9,3 +11,15 @@ const ubahWarna2 = () => {
 }
 
 paragraph2.onclick = ubahWarna2
+
+// Add Event Listener
+paragraph4.addEventListener('click', () => {
+    const ul = document.querySelector('section#b ul')
+    const newList = document.createElement('li')
+    const newTeksList = document.createTextNode('new list')
+
+    newList.appendChild(newTeksList)
+    ul.appendChild(newList)
+
+
+})
