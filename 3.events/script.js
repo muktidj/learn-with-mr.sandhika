5ed503801f090cb3cba402166817f39928a43446
muktidj <muktidwijatmoko@gmail.com>
2019-12-22 14:46:07 +0700

    Bab event, menggunakan event handler dengan cara elemen method pada script.js

--- 3.events/script.js
+++ 3.events/script.js
@@ -1,5 +1,11 @@
 const paragraph3 = document.querySelector('.p3')
+const paragraph2 = document.querySelector('.p2')
 
-const ubahWarna = () => {
+const ubahWarna3 = () => {
     paragraph3.style.backgroundColor = 'coral'
 }
+const ubahWarna2 = () => {
+    paragraph2.style.backgroundColor = 'salmon'
+}
+
+paragraph2.onclick = ubahWarna2
