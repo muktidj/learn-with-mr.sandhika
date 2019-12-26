5be7a0f70503b834b772a4362e1988f1d9a01e23
muktidj <muktidwijatmoko@gmail.com>
2019-12-26 17:45:57 +0700

    Memahami cara Kerja Closure

--- js-object/script.js
+++ js-object/script.js
@@ -1,82 +1,51 @@
-/*2 Fase pada Execution Context
-1. Creation
-2.Execution
+//Closure
+// function init() {
+//   // let nama = 'Mukti'
+//   return function(nama) {
+//     console.log(nama);
 
-2.1 Execution Context, Hoisting && Scope
-*/
-
-// console.log(nama);
-// var nama = "Mukti";
-
-// Creation Phase pada Global Context
-// Nyari apakah ada keyword variabel, kalo ada dia membuat nama variabel di set 'undefined'
-// nama function = fn()
-//hoisitng
-// itu yang terjadi pada Executian Context
-
-// Exucution Phase
-
-//  var nama = 'Mukti'
-//  var age = 21
-
-//  console.log(sayHello());
-
-//  function sayHello() {
-//   return `WOI ${nama}, umur saya ${age}`
-//  }
-
-
-// function membuat local execution context
-// yg didalamnya terdapat creation dan execution window
-//arguments
-//hoisting
-
-// Scope
-
-
-// var nama = 'Mukti'
-// var age = 21
-
-// function gabungNama() {
-//   console.log(arguments);
-
-//   let introduce = 'perkenalkan'
-//   return `${introduce}, nama saya ${nama}. Umur saya ${age}`
-// }
-
-// console.log(gabungNama('Ayu Retno'));
-
-
-// function a() {
-//   console.log('Ini A');
-
-//   function b() {
-//     console.log('Ini B');
-
-//     function c() {
-//       console.log('Ini C');
-
-//     }
-//     c()
 //   }
-//   b()
+
 // }
-// a()
 
-// Hoisting && Scope
-function satu() {
-  var nama = "ghost"
-  console.log(nama);
+// let panggilNama = init()
+// panggilNama('Ayu')
+// panggilNama('Retno')
 
-}
 
-function dua() {
-  console.log(nama);
+// const ucapkanSalam = (waktu) => {
+//   return (nama) => {
+//     console.log(`Halo ${nama}, good ${waktu}. Have Nice Day`);
 
-}
+//   }
+// }
 
-console.log(nama);
-var nama = "Ayu"
-satu()
-dua('dinda')
-console.log(nama);
+// const goodMorning = ucapkanSalam('Morning')
+// const goodAfternoon = ucapkanSalam('Afternoon')
+// const goodNight = ucapkanSalam('Night')
+
+// goodMorning('Mukti')
+// goodAfternoon('Dwi')
+// goodNight('Jatmoko')
+
+// console.dir(goodNight())
+
+
+
+let add = (() => {
+  let counter = 0
+  return function() {
+    return ++counter;
+  }
+})();
+
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+
+// let a = add()
+// console.log(a());
+// console.log(a());
+// console.log(a());
+// console.log(a());
