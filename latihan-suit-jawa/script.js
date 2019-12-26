a15c6a080905fb6cf927f1f4c308063b8ad29eb5
muktidj <muktidwijatmoko@gmail.com>
2019-12-26 13:25:08 +0700

    Membuat kode suwit Jawa Lebih Efektif cara 2

--- latihan-suit-jawa/script.js
+++ latihan-suit-jawa/script.js
@@ -1,75 +1,107 @@
-const playerGajah = document.querySelector('.gajah');
-const playerOrang = document.querySelector('.orang');
-const playerSemut = document.querySelector('.semut');
+const playerGajah = document.querySelector(".gajah");
+const playerOrang = document.querySelector(".orang");
+const playerSemut = document.querySelector(".semut");
 
 function getPilihanKomputer() {
-    // menangkap pilihan computer
-    // membangkitkan bilangan random
-   const comp = Math.random();
+  // menangkap pilihan computer
+  // membangkitkan bilangan random
+  const comp = Math.random();
 
-    if( comp < 0.34 ) return 'gajah';
-    if( comp >= 0.34 && comp < 0.67 ) return 'orang'
-    return 'semut';
+  if (comp < 0.34) return "gajah";
+  if (comp >= 0.34 && comp < 0.67) return "orang";
+  return "semut";
+}
+
+function getHasil(comp, player) {
+  if (player === comp) return "SERI!";
+  if (player === "gajah") return comp === "orang" ? "MENANG!" : "KALAH!";
+  if (player === "orang") return comp === "gajah" ? "KALAH!" : "MENANG!";
+  if (player === "semut") return comp === "orang" ? "KALAH" : "MENANG!";
+}
+
+function putar() {
+  const imgKomputer = document.querySelector(".img-komputer");
+  const gambar = ["gajah", "orang", "semut"];
+  let i = 0;
+  const waktuMulai = new Date().getTime();
+  setInterval(() => {
+    if (new Date().getTime() - waktuMulai > 1000) {
+      clearInterval;
+      return;
     }
-
-function  getHasil(comp, player) {
-    if( player === comp ) return 'SERI!';
-    if( player === 'gajah' ) return ( comp === 'orang' ) ? 'MENANG!' : 'KALAH!';
-    if( player === 'orang' ) return ( comp === 'gajah' ) ? 'KALAH!' : 'MENANG!';
-    if( player === 'semut' ) return ( comp === 'orang' ) ? 'KALAH' : 'MENANG!';
-
+    imgKomputer.setAttribute("src", `img/${gambar[i++]}.png`);
+    if (i === gambar.length) i = 0;
+  }, 100);
 }
 
-function playerPilihGajah() {
+const pilihanBanyak = document.querySelectorAll("li img");
+pilihanBanyak.forEach(pilihan => {
+  pilihan.addEventListener("click", () => {
+    const pilihanKomputer = getPilihanKomputer();
+    const pilihanPlayer = pilihan.className;
+    const hasil = getHasil(pilihanKomputer, pilihanPlayer);
 
-    playerGajah.addEventListener('click', () => {
-        const pilihanKomputer = getPilihanKomputer();
-        const pilihanPlayer = playerGajah.className
-        const hasil = getHasil(pilihanKomputer, pilihanPlayer)
-        const imgKomputer = document.querySelector('.img-komputer')
-        imgKomputer.setAttribute('src', `img/${pilihanKomputer}.png`)
-        const info = document.querySelector('.info ')
-        info.innerHTML = hasil
-        console.log('comp :' + pilihanKomputer);
-        console.log('player :' + pilihanPlayer);
-        console.log('hasil :' + hasil);
+    putar();
 
-    })
-}
+    setTimeout(() => {
+      const imgKomputer = document.querySelector(".img-komputer");
+      imgKomputer.setAttribute("src", `img/${pilihanKomputer}.png`);
+      const info = document.querySelector(".info ");
+      info.innerHTML = hasil;
+      console.log("comp :" + pilihanKomputer);
+      console.log("player :" + pilihanPlayer);
+      console.log("hasil :" + hasil);
+    }, 1000);
+  });
+});
 
-function playerPilihOrang() {
-    playerOrang.addEventListener('click', () => {
-        const pilihanKomputer = getPilihanKomputer();
-        const pilihanPlayer = playerOrang.className
-        const hasil = getHasil(pilihanKomputer, pilihanPlayer)
-        const imgKomputer = document.querySelector('.img-komputer')
-        imgKomputer.setAttribute('src', `img/${pilihanKomputer}.png`)
-        const info = document.querySelector('.info ')
-        info.innerHTML = hasil
-        console.log('comp :' + pilihanKomputer);
-        console.log('player :' + pilihanPlayer);
-        console.log('hasil :' + hasil);
+// function playerPilihGajah() {
+//   playerGajah.addEventListener("click", () => {
+//     const pilihanKomputer = getPilihanKomputer();
+//     const pilihanPlayer = playerGajah.className;
+//     const hasil = getHasil(pilihanKomputer, pilihanPlayer);
+//     const imgKomputer = document.querySelector(".img-komputer");
+//     imgKomputer.setAttribute("src", `img/${pilihanKomputer}.png`);
+//     const info = document.querySelector(".info ");
+//     info.innerHTML = hasil;
+//     console.log("comp :" + pilihanKomputer);
+//     console.log("player :" + pilihanPlayer);
+//     console.log("hasil :" + hasil);
+//   });
+// }
 
-    })
-}
+// function playerPilihOrang() {
+//     playerOrang.addEventListener('click', () => {
+//         const pilihanKomputer = getPilihanKomputer();
+//         const pilihanPlayer = playerOrang.className
+//         const hasil = getHasil(pilihanKomputer, pilihanPlayer)
+//         const imgKomputer = document.querySelector('.img-komputer')
+//         imgKomputer.setAttribute('src', `img/${pilihanKomputer}.png`)
+//         const info = document.querySelector('.info ')
+//         info.innerHTML = hasil
+//         console.log('comp :' + pilihanKomputer);
+//         console.log('player :' + pilihanPlayer);
+//         console.log('hasil :' + hasil);
 
-function playerPilihSemut() {
-    playerSemut.addEventListener('click', () => {
-        const pilihanKomputer = getPilihanKomputer();
-        const pilihanPlayer = playerSemut.className
-        const hasil = getHasil(pilihanKomputer, pilihanPlayer)
-        const imgKomputer = document.querySelector('.img-komputer')
-        imgKomputer.setAttribute('src', `img/${pilihanKomputer}.png`)
-        const info = document.querySelector('.info ')
-        info.innerHTML = hasil
-        console.log('comp :' + pilihanKomputer);
-        console.log('player :' + pilihanPlayer);
-        console.log('hasil :' + hasil);
-    })
+//     })
+// }
 
-}
+// function playerPilihSemut() {
+//     playerSemut.addEventListener('click', () => {
+//         const pilihanKomputer = getPilihanKomputer();
+//         const pilihanPlayer = playerSemut.className
+//         const hasil = getHasil(pilihanKomputer, pilihanPlayer)
+//         const imgKomputer = document.querySelector('.img-komputer')
+//         imgKomputer.setAttribute('src', `img/${pilihanKomputer}.png`)
+//         const info = document.querySelector('.info ')
+//         info.innerHTML = hasil
+//         console.log('comp :' + pilihanKomputer);
+//         console.log('player :' + pilihanPlayer);
+//         console.log('hasil :' + hasil);
+//     })
 
+// }
 
-playerPilihSemut()
-playerPilihGajah()
-playerPilihOrang()
+// playerPilihSemut()
+// playerPilihGajah()
+// playerPilihOrang()
