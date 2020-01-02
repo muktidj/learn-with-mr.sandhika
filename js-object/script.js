3eb4245150127c3f92d7d47d9fbf277d9cd4158b
muktidj <muktidwijatmoko@gmail.com>
2020-01-02 12:51:03 +0700

    3.1 Arrow Function Perbedaanya jika menggunakan konsep this

--- js-object/script.js
+++ js-object/script.js
@@ -17,4 +17,29 @@ const tampilNama2 = (nama, waktu) => {
 let child = ['Ibrahim Mukti', 'Sulleyman Mukti', 'Mutia Ayu']
 
 // console.log(child.map(children => children.length))
-console.table(child.map(children => ({nama: children, longNama : children.length})))
+// console.table(child.map(children => ({nama: children, longNama : children.length})))
+
+// Constructor Function
+const Mahasiswa = function() {
+  this.nama = 'Mukti DJ'
+  this.age = 21
+  this.sayHello = function()  {
+    // console.log(`Halo ${this.nama}, umur saya ${this.age}`);
+
+  }
+
+// Perbedaan arrow function dan function biasa pada setInterval akan mengacu pada windows
+// kalo arrow function akan mengacu pada lexical
+
+// setInterval(function() {
+//   console.log(this);
+
+// }, 1000);
+setInterval(() => {
+  console.log(this.age++);
+
+}, 4000);
+
+}
+
+const mukti = new Mahasiswa()
